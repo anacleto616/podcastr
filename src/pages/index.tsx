60469822865070efb76c0f0db4e442998c90c3fb
Rafael Anacleto <rafael.anacleto12@gmail.com>
2021-04-22 08:18:27 -0300

    Aula 2

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+// SPA - uso do useEffect() com fetch, por exemplo
+// SSR - export async function getServerSideProps()
+// SSG - export async function getStaticProps()
+
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes")
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
